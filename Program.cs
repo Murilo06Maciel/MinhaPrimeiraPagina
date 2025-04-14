86ec929c64eba5911a85edcdaf0143bd00395971
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-04-14 14:08:40 -0300

    atualizando atividade ead

--- Program.cs
+++ Program.cs
@@ -3,4 +3,12 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseDefaultFiles(); // Serve index.html por padrão
+app.UseStaticFiles(); // Permite servir arquivos da pasta wwwroot
+
+app.MapGet("/sobre", async context =>
+{
+await context.Response.SendFileAsync("wwwroot/sobre.html");
+});
+
 app.Run();
